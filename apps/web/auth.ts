364476fb11ff22b3713f6cda0847755dd51f194a
Julio Garcia <jsgarcia.dev@gmail.com>
2025-02-06 09:51:53 -0300

    docs: add todo for email verification implementation in login

--- apps/web/auth.ts
+++ apps/web/auth.ts
@@ -7,6 +7,14 @@ import { getUserById } from '@/data/user';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   callbacks: {
+    // TODO FEAT: Implementar a verificação de email no login
+    // async signIn({ user }) {
+    //   const existingUser = await getUserById(user.id as string);
+
+    //   if (!existingUser || !existingUser.emailVerified) return false;
+    //   return true;
+    // },
+
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
