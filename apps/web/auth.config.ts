f351604531df585af2ccd559f803c75b22051bc6
Julio Garcia <jsgarcia.dev@gmail.com>
2025-02-06 10:24:09 -0300

    feat: add google oauth provider and signin button

--- apps/web/auth.config.ts
+++ apps/web/auth.config.ts
@@ -1,12 +1,17 @@
 import bcrypt from 'bcryptjs';
 import type { NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import Google from 'next-auth/providers/google';
 
 import { loginSchema } from '@/schemas';
 import { getUserByEmail } from '@/data/user';
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = loginSchema.safeParse(credentials);
--- apps/web/components/auth/social.tsx
+++ apps/web/components/auth/social.tsx
@@ -1,22 +1,31 @@
 'use client';
 
+import { signIn } from 'next-auth/react';
 import GoogleIcon from '@/components/icons/google-icon';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
-import { Github } from 'lucide-react';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 
 interface SocialProps {
   className?: string;
 }
 
 export const Social = ({ className }: SocialProps) => {
+  const onClick = (provider: 'google' | 'github') => {
+    signIn(provider, {
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
   return (
     <div className="flex w-full items-center gap-x-2">
-      <Button size="lg" variant="outline" onClick={() => {}} className={cn('w-full', className)}>
+      <Button
+        size="lg"
+        variant="outline"
+        onClick={() => onClick('google')}
+        className={cn('w-full border-lime-500/20', className)}
+      >
         <GoogleIcon className="size-5" />
-      </Button>
-      <Button size="lg" variant="outline" onClick={() => {}} className={cn('w-full', className)}>
-        <Github className="size-5" />
+        Signin with google
       </Button>
     </div>
   );
